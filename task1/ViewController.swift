cd0b4108310c75a2ab4c61143d495a635dcda61f
altynbekova <gulzhanaltynbekova@gmail.com>
2024-09-28 21:44:47 +0700

    first commit

---
+++ task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task1
+//
+//  Created by Nurgali Altynbekov on 28.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
